3a1b017212468e9b173df1d316ec6211fab345a7
ZHcoths <war3zhuhe@gmial.com>
2018-10-09 22:58:40 +0800

    41-46

---
+++ src/swordtoOffer/FindContinuousSequence_41.java
@@ -0,0 +1,25 @@
+package swordtoOffer;
+
+import java.util.ArrayList;
+
+/**
+ * @program: sort
+ * @author: zhuhe
+ * @create: 2018-10-09 16:19
+ **/
+
+public class FindContinuousSequence_41 {
+    public ArrayList<ArrayList<Integer> > FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for (int n = (int) Math.sqrt(2 * sum); n >= 2; n--) {
+            if ((n & 1) == 1 && sum % n == 0 || (sum % n) * 2 == n) {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int j = 0, k = (sum / n) - (n - 1) / 2; j < n; j++, k++) {
+                    list.add(k);
+                }
+                ans.add(list);
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/swordtoOffer/ReverseSentence_44.java
@@ -0,0 +1,10 @@
+package swordtoOffer;
+
+/**
+ * @program: sort
+ * @author: zhuhe
+ * @create: 2018-10-09 21:31
+ **/
+public class ReverseSentence_44 {
+    //不做了
+}
